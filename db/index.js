040cd81f9ec2f5cd957d3af0a66cb9925c280fa6
Santiago <santiagofgaray@gmail.com>
2022-02-20 00:32:57 +0100

    lab finished

--- db/index.js
+++ db/index.js
@@ -1,11 +1,12 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
+require("dotenv").config()
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+const MONGO_URI = process.env.MONGODB_URI || `mongodb+srv://${process.env.MG_USERNAME}:${process.env.MG_PWD}@cluster0.6fts6.mongodb.net/lab-express-drones2`;
 
 mongoose
   .connect(MONGO_URI)
--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,50 @@
 const express = require('express');
+const { find, findByIdAndUpdate, findById } = require('../models/Drone.model');
+const mongoose = require("mongoose");
+
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require("../models/Drone.model")
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+  .then((drones) => res.render("drones/list", {drones} ))
 });
 
-router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.route('/drones/create')    // Iteration #3: Add a new drone
+  .get((req, res, next) => {   
+    res.render("drones/create-form")
+})
+  .post((req, res, next) => {
+  const {name, propellers, maxSpeed} = req.body  
+  Drone.create ({name, propellers, maxSpeed})
+    .then(() =>{res.redirect("/drones")})
 });
 
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+
+router.route('/drones/:id/edit')  // Iteration #4: Update the drone
+  .get((req, res, next) => {
+    id=req.params.id
+    Drone.findById(id)
+      .then((drone) => {
+        res.render("drones/update-form", drone)
+      })
+})
+  .post( (req, res, next) => {
+    id=req.params.id
+    const {name, propellers, maxSpeed} = req.body
+    Drone.findByIdAndUpdate(id, {name, propellers, maxSpeed})
+    .then(() => {
+      res.redirect("/drones")
+  })
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
-});
-
-router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
-});
-
-router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+router.post('/drones/:id/delete', (req, res, next) => {    // Iteration #5: Delete the drone
+  id=req.params.id
+  Drone.findByIdAndDelete(id)
+  .then(() => {res.redirect("/drones")})
 });
 
 module.exports = router;
